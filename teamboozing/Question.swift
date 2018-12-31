680a0b189de2eb0fbcbb0a61432a714749252f5e
Patrik Hora <hora@master.cz>
2018-12-31 12:40:27 +0100

    Add more silvester questions

--- teamboozing/Question.swift
+++ teamboozing/Question.swift
@@ -42,26 +42,67 @@ class Question {
     }
     
     static var allQuestions: [Question] {
-        return originalQuestions + silvesterQuestions
+        return /*originalQuestions +*/ silvesterQuestions
     }
     
+    // Thanks to Koty and https://conversationstartersworld.com/truth-dare-questions/?fbclid=IwAR0FwOKVWdzM1OlNtQAy3v9S2enIfQdSuXjQZPgJVSXNFJkWSQ9T88qkhTo for many of the questions below
     static let silvesterQuestions = [
-        Question("sundej si tričko", difficulty: 3)
+        Question("kdybys byl/a opačné sexuality, s kterým hráčem bys to chtěl/a dělat?"),
+        Question("co nejdražšího jsi ukradl/a?"),
+        Question("popiš detailně fotku, na které vypadáš nejtrapněji"),
+        Question("přiznej svůj netrapnější zlozvyk"),
+        Question("popiš svůj nejdivočejší zážitek s policií"),
+        Question("čeho nebo se nejvíce bojíš?"),
+        Question("popiš nevhodnou situaci, ve které sis prdnul/a"),
+        Question("proč ses rozešel/rozešla s předchozím přítelem?"),
+        Question("kdo z hráčů je nejvíce sexy? A kdo je druhý a třetí nejvíce sexy?"),
+        Question("co nejdivnějšího jsi při sexu dělal/a?", difficulty: 3),
+        Question("řekni o sobě něco co nikdo z přítomných neví"),
+        Question("jakou nejtrapnější přezdívku jsi měl/a?"),
+        Question("popiš nejdivnější příhodu kdy jsi zvracel/a"),
+        Question("co bys dělal/a kdyby ses na jeden den probudil/a jako opačné pohlaví?", difficulty: 1),
+        Question("tancuj 30 sekund bez hudby", difficulty: 3),
+        Question("nech ostatní hráče aby tě jakkoliv napózovali a vyfotili se s tebou"),
+        Question("ztvárni svůj život krátkým interpretačním tancem"),
+        Question("olízni podlahu", difficulty: 3),
+        Question("vypij panáka kterého ti vytvoří a namíchají ostatní", difficulty: 4),
+        Question("nech ostatní hráče, aby ti napsali fixem něco na tělo", difficulty: 3),
+        Question("nech ostatní hráče, aby ti udělali jakýkoliv účes"),
+        Question("breakdancuj 30 sekund"),
+        Question("dej osobě po levici pusu"),
+        Question("předveď červa, na zemi"),
+        Question("pokus se o kouzelnický trik"),
+        Question("vyměň si oblečení s někým ve skupině na další 3 kola"),
+        Question("přejdi z jednoho konce pokoje na druhý po rukou, někdo ti může chytit nohy"),
+        Question("ptáčejte kolem sebe imaginarní hula hop po 2 kola"),
+        Question("tancuj na celou písničku podle výběru ostatních"),
+        Question("běž na záchod, sundej si trenky/kalhotky a nos je na hlavě až do konce hry", difficulty: 5),
+        Question("sněz 15 SECALO do 30 sekund, bez zapíjení"),
+        Question("sundej si tričko", difficulty: 4),
+        Question("dej si panáka něčeho alespoň 40%", difficulty: 3),
+        Question("řekni, který z mužských hráčů má podle tebe nejmenší penis"),
+        Question("udělej 5 kliků"),
+        Question("řekni křestní jména všech hráčů"),
+        Question("řekni, kterému hráči podle tebe nejvíc smrdí nohy"),
+        // MARK: Needs Opponent
+        Question("chovej se 30 sekund jako zvíře, které vybere <SOUPER>", needsOpponent: true),
+        Question("slož krátkou báseň na základě tří slov, které vybere <SOUPER>", needsOpponent: true),
+        Question("nech <SOUPER>, aby ti napráskal/a na zadek", difficulty: 3, needsOpponent: true),
+        Question("postni na Facebook fotku dle svého výběru z pěti fotek které ti nafotí <SOUPER> na tvůj telefon", difficulty: 3, needsOpponent: true),
+        Question("prodej <SOUPER> nějakou sračku, použij k tomu své nejlepší prodejní schopnosti", needsOpponent: true),
+        Question("tanči na písničku, kterou vybere <SOUPER>, jako by na tom závisel tvůj život", difficulty: 3, needsOpponent: true),
+        Question("má <SOUPER> radši vodku nebo rum?", needsOpponent: true),
+        //MARK: All
+        Question("Napij se!", type: .all),
+        Question("Vyplázni jazyk!", type: .all)
     ]
     
     static let originalQuestions = [
         Question("jmenuj pět dobrých vlastností svého šéfa", difficulty: 3),
-        Question("kdy má <SOUPER> narozeniny?", needsOpponent: true),
-        Question("jakého má domácího mazlíčka <SOUPER>?", difficulty: 1, needsOpponent: true),
-        Question("řekni třikrát za sebou správně jazykolam \"Pudl prdl pudr\"", difficulty: 1),
-        Question("popiš náplň práce <SOUPER>", difficulty: 1, needsOpponent: true),
-        Question("co je nejoblíbenější jídlo <SOUPER>?", needsOpponent: true),
-        Question("co je nejoblíbenější alkoholický nápoj <SOUPER>?", needsOpponent: true),
         Question("řekni své jméno pozpátku", difficulty: 1),
-        Question("vyhláskuj jméno <SOUPER>", difficulty: 1, needsOpponent: true),
+        Question("řekni třikrát za sebou správně jazykolam \"Pudl prdl pudr\"", difficulty: 1),
         Question("který hráč je nejmenší?"),
         Question("který hráč je nejvyšší?"),
-        Question("co je nejoblíbenější volnočasová činnost <SOUPER>?", needsOpponent: true),
         Question("seřaď kolegy podle doby působení ve firmě", difficulty: 3),
         Question("vyjmenuj hráče, kteří mají děti"),
         Question("který hráč bydlí nejdále od firmy?", difficulty: 3),
@@ -81,10 +122,8 @@ class Question {
         Question("přeběhni místnost tam a zpátky a přitom volej \"Run Forrest, run!\"", difficulty: 3),
         Question("zamňoukej, zaštěkej a zatrub jako slon", difficulty: 1),
         Question("vyfoť týmové selfie, kde všichni dělají duck face", difficulty: 3),
-        Question("odpověz pravdivě na otázku od <SOUPER> dle jejich výběru", needsOpponent: true),
         Question("řekni vtip, který rozesměje někoho z jiného týmu"),
         Question("vyskákej na jedné noze dvě kolečka kolem své vlastní osy a volej u toho: \"Jsem hvězda!\""),
-        Question("<SOUPER> řekne jednu pravdu a jednu lež. Uhádni co byla pravda", difficulty: 3, needsOpponent: true),
         Question("zatancuj exotický tanec"),
         Question("napij se z nápoje souseda po pravici", difficulty: 1),
         Question("napij se z nápoje souseda po levici", difficulty: 1),
@@ -95,6 +134,17 @@ class Question {
         Question("popiš náplň svojí práce se silným indickým přízvukem"),
         Question("řekni abecedu pozpátku do 30 sekund", difficulty: 3),
         Question("vyměň si nápoj s někým ze svého týmu"),
+        // MARK: Needs Opponent
+        Question("<SOUPER> řekne jednu pravdu a jednu lež. Uhádni co byla pravda", difficulty: 3, needsOpponent: true),
+        Question("odpověz pravdivě na otázku od <SOUPER> dle jejich výběru", needsOpponent: true),
+        Question("popiš náplň práce <SOUPER>", difficulty: 1, needsOpponent: true),
+        Question("co je nejoblíbenější jídlo <SOUPER>?", needsOpponent: true),
+        Question("co je nejoblíbenější alkoholický nápoj <SOUPER>?", needsOpponent: true),
+        Question("vyhláskuj jméno <SOUPER>", difficulty: 1, needsOpponent: true),
+        Question("jakého má domácího mazlíčka <SOUPER>?", difficulty: 1, needsOpponent: true),
+        Question("kdy má <SOUPER> narozeniny?", needsOpponent: true),
+        Question("co je nejoblíbenější volnočasová činnost <SOUPER>?", needsOpponent: true),
+        // MARK: All
         Question("Sedni si na zem!", type: .all),
         Question("Lehni si na zem!", type: .all),
         Question("Zvedni levou ruku!", type: .all),
